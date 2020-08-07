4a9ce5deb25e01020b132412fcea18b409fecd83
metehus <methidezeeni@gmail.com>
2020-08-07 18:58:59 -0300

    gh pages

--- src/components/MessageHeader.jsx
+++ src/components/MessageHeader.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 import VerifiedIcon from '../assets/verified'
 
--- src/components/MessageWrapper.jsx
+++ src/components/MessageWrapper.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 
 const MessageWrapper = styled.div`
-  a: ${p => console.log(p)};
   background-color: ${props => props.theme.palette.background};
   color: ${props => props.theme.palette.text};
   font-family: ${props => props.theme.typography.text}
