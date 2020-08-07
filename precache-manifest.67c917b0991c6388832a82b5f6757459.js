61cac8ae4ff327ce831d53214d03dd774f34455a
metehus <methidezeeni@gmail.com>
2020-08-07 18:43:45 -0300

    Updates

---
+++ precache-manifest.67c917b0991c6388832a82b5f6757459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb8a6724c32eaccc5d7b555c1ed28fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ae59e022ebd2ff107a2",
+    "url": "./static/css/main.44aa286a.chunk.css"
+  },
+  {
+    "revision": "27b538ab7b2d4a38b2af",
+    "url": "./static/js/2.1644adb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1644adb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae59e022ebd2ff107a2",
+    "url": "./static/js/main.2e17a4f4.chunk.js"
+  },
+  {
+    "revision": "799fc59a22455ae9787f160cb1809238",
+    "url": "./static/js/main.2e17a4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5767973052cb36fff5f",
+    "url": "./static/js/runtime-main.48a89c92.js"
+  }
+]);
