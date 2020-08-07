602c76af2e162f94054b57ecac9af4434def70a1
metehus <methidezeeni@gmail.com>
2020-08-07 00:10:14 -0300

    create react library

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-discord-components'
+import 'react-discord-components/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
